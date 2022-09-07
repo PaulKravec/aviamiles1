4468c7bf8d80a80804e2a821892b9bad151ad891
Paul_RazDva <paul1312white@gmail.com>
2022-09-07 23:24:26 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+      int x = 20 * 1; // рублей за 1 милю
+      int y = 6000; // стоимость авиабилета
+      int z = y/x; // сколько миль вышло за стоимость билета
+        System.out.println(z);
+    }
+}
