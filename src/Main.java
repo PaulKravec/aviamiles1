3ce0ee184e4f90ddb636e820383ccc741a7bc783
Paul_RazDva <paul1312white@gmail.com>
2022-09-17 18:27:32 +0300

    java_style

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-      int x = 20 * 1; // рублей за 1 милю
-      int y = 6000; // стоимость авиабилета
-      int z = y/x; // сколько миль вышло за стоимость билета
-        System.out.println(z);
+        int costPerMile = 20 * 1; // рублей за 1 милю
+        int airfare = 6000; // стоимость авиабилета
+        int milesFromTicket = airfare / costPerMile; // сколько миль вышло за стоимость билета
+        System.out.println(milesFromTicket);
     }
 }
